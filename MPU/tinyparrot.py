a1bf175ebea5ee7429785c314c93388f11033dce
Cornifer <lopcrecom@gmail.com>
2026-09-01 23:38:57 +0200

    Debug code

--- MPU/tinyparrot.py
+++ MPU/tinyparrot.py
@@ -5,5 +5,31 @@ from arduino_bridge import Bridge
 from edge_impulse_linux.image import ImageImpulseRunner
 
 
+# Load the model
 runner = ImageImpulseRunner("model/parrot_model.eim")
 runner.init()
+
+
+# Grab the first camera
+camera = cv2.VideoCapture(0)
+
+
+# Just debugging for now
+def on_button_event(state):
+    if state == "pressed":
+        print("Recording audio...")
+    elif state == "realased":
+        print("Saving audio...")
+
+
+# Connect to MCU bridge listener
+bridge = Bridge()
+bridge.on("button_event", on_button_event)
+
+
+while True:
+    # Keep checking MCU events
+    bridge.poll() # 
+
+
+    #TODO: Add the image recognition things...
---
+++ README.md
@@ -0,0 +1,5 @@
+
+- MCU : C++ Code for STM32 MCU
+- MPU : Python Script for Qualcomm MPU
+- Model : Edge impulse binary files
+- Design : 3D Models of the physical parrot
